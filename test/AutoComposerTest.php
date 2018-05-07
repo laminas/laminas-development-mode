8f590753dc404243ef13cd03d1debebebf33f1b1
Matthew Weier O'Phinney <matthew@zend.com>
2018-05-07 10:40:08 -0500

    Ensure AutoComposerTest extends namespaced PHPUnit TestCase

--- test/AutoComposerTest.php
+++ test/AutoComposerTest.php
@@ -9,7 +9,7 @@ namespace ZFTest\DevelopmentMode;
 
 use org\bovigo\vfs\vfsStream;
 use org\bovigo\vfs\vfsStreamContainer;
-use PHPUnit_Framework_TestCase as TestCase;
+use PHPUnit\Framework\TestCase;
 use ZF\DevelopmentMode\AutoComposer;
 
 class AutoComposerTest extends TestCase
