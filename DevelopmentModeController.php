7f922fc4309fde62193aea18d88ab7ae43924337
Matthew Weier O'Phinney <matthew@weierophinney.net>
2019-12-31 10:40:04 -0600

    Rewriting as Laminas Project package

--- DevelopmentModeController.php
+++ DevelopmentModeController.php
@@ -1,14 +1,16 @@
 <?php
+
 /**
- * @license   http://opensource.org/licenses/BSD-3-Clause BSD-3-Clause
- * @copyright Copyright (c) 2014 Zend Technologies USA Inc. (http://www.zend.com)
+ * @see       https://github.com/laminas/laminas-development-mode for the canonical source repository
+ * @copyright https://github.com/laminas/laminas-development-mode/blob/master/COPYRIGHT.md
+ * @license   https://github.com/laminas/laminas-development-mode/blob/master/LICENSE.md New BSD License
  */
 
-namespace ZF\DevelopmentMode;
+namespace Laminas\DevelopmentMode;
 
-use Zend\Console\Request as ConsoleRequest;
-use Zend\EventManager\EventManagerInterface;
-use Zend\Mvc\Controller\AbstractActionController;
+use Laminas\Console\Request as ConsoleRequest;
+use Laminas\EventManager\EventManagerInterface;
+use Laminas\Mvc\Controller\AbstractActionController;
 
 class DevelopmentModeController extends AbstractActionController
 {
